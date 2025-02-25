f5cf22084d30a36e2d938040e043764d5418599f
apperly <jim.apperly@gmail.com>
2025-02-25 15:35:19 +0000

    refinement of styles; improve nav responsiveness

--- js/site-nav.js
+++ js/site-nav.js
@@ -2,14 +2,30 @@
  * Copyright (c) 2025 UsefulWorks Ltd. All Rights Reserved.
  */
 
-document.readyState =="loading" ? window.addEventListener("load", setupNav) : setupNav();
-
-function setupNav() {
-    const MENU_BTN = document.getElementById("navbar-menu-button");
-    const NAV_MENU = document.querySelector("nav.top-nav ul.navbar-links");
-    if (MENU_BTN && NAV_MENU) {
-        MENU_BTN.addEventListener("click", function(e) {
-            NAV_MENU.classList.toggle("shown");
-        });
+const NavBar = {
+    init : function() {
+        const SHOWING_CNAME = "showing";
+        const MENU_BTN = document.getElementById("navbar-menu-button");
+        const NAV_MENU = document.querySelector("nav.top-nav ul.navbar-links");
+        if (MENU_BTN && NAV_MENU) {
+            MENU_BTN.addEventListener("click", function(e) {
+                NAV_MENU.classList.toggle(SHOWING_CNAME);
+            });
+        }
+        if (window) {
+            window.addEventListener("scroll", NavBar.windowScroll);
+        }
+    },
+    windowScroll : function(e) {
+        const TOP_NAV_SLIM_CNAME = "top-nav-slim";
+        const NAVBAR = document.querySelector("nav.top-nav");
+        if (NAVBAR) {
+            with(NAVBAR.classList) {
+                window.scrollY > 50 ? add(TOP_NAV_SLIM_CNAME) : remove(TOP_NAV_SLIM_CNAME);
+            }
+       }
     }
-}
+};
+window.addEventListener("load", NavBar.init);
+
+
