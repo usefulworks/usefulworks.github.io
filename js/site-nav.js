156a0cf1c67dfe0222ae5f3661ae33285552e043
apperly <jim.apperly@gmail.com>
2025-02-26 00:50:26 +0000

    add basic contact form as section to index.html

--- js/site-nav.js
+++ js/site-nav.js
@@ -4,28 +4,34 @@
 
 const NavBar = {
     init : function() {
-        const SHOWING_CNAME = "showing";
-        const MENU_BTN = document.getElementById("navbar-menu-button");
-        const NAV_MENU = document.querySelector("nav.top-nav ul.navbar-links");
-        if (MENU_BTN && NAV_MENU) {
-            MENU_BTN.addEventListener("click", function(e) {
-                NAV_MENU.classList.toggle(SHOWING_CNAME);
-            });
-        }
-        if (window) {
-            window.addEventListener("scroll", NavBar.windowScroll);
-        }
-    },
-    windowScroll : function(e) {
-        const TOP_NAV_SLIM_CNAME = "top-nav-slim";
-        const NAVBAR = document.querySelector("nav.top-nav");
-        if (NAVBAR) {
-            with(NAVBAR.classList) {
-                window.scrollY > 50 ? add(TOP_NAV_SLIM_CNAME) : remove(TOP_NAV_SLIM_CNAME);
+        console.log("NavBar.init");
+        const initNavMenuToggle = function () {
+            console.log("NavBar.initNavMenuToggle");
+            const SHOWING_CNAME = "showing";
+            const MENU_BTN = document.getElementById("navbar-menu-button");
+            const NAV_MENU = document.querySelector("nav.top-nav ul.navbar-links");
+            if (MENU_BTN && NAV_MENU) {
+                MENU_BTN.addEventListener("click", function(e) {
+                    NAV_MENU.classList.toggle(SHOWING_CNAME);
+                });
             }
-       }
+
+        };
+        const initWindowScroll = function() {
+            console.log("NavBar.initWindowScroll");
+            const TOP_NAV_SLIM_CNAME = "top-nav-slim";
+            const NAVBAR = document.querySelector("nav.top-nav");
+            const HANDLER = function() {
+                if (NAVBAR) {
+                    with(NAVBAR.classList) {
+                        window.scrollY > 50 ? add(TOP_NAV_SLIM_CNAME) : remove(TOP_NAV_SLIM_CNAME);
+                    }
+            }
+            };
+            window.addEventListener("scroll", HANDLER) && HANDLER.call();
+        }
+        initNavMenuToggle();
+        initWindowScroll();
     }
 };
 window.addEventListener("load", NavBar.init);
-
-
