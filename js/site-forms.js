ca3cead7576ad2f641b64fd13700ae0bf51af1a2
apperly <jim.apperly@gmail.com>
2025-02-28 14:54:37 +0000

    taming recaptch configuration

--- js/site-forms.js
+++ js/site-forms.js
@@ -6,14 +6,15 @@ const Forms = {
 
     init : function() {
         console.log("Forms.init");
-        console.log("$UW.myName" + $UW.myName);
+        console.log("$UW.myName is " + $UW.myName);
+        Forms.renderRecaptcha();
     },
 
-    Recaptcha : {
+    renderRecaptcha : function() {
         // the external recaptcha/api.js must be loaded *after* this def
-        load : function() {
-            console.log("Forms.Recaptcha.load");
-            //grecaptcha.render("g-recaptcha", { "sitekey": "{{ sitekey }}" });
-        }
+        // window.onload waits for the script to load (document on load does not)
+
+        grecaptcha.render("g-recaptcha", { "sitekey": "{{ sitekey }}" });
     }
 };
+window.addEventListener("load", Forms.init);
