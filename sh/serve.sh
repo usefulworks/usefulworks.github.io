ab0ad8d1f83b63b47276db5672934ae99afd24a8
apperly <jim.apperly@gmail.com>
2025-03-01 02:30:45 +0000

    when you're styling, the whole world styles with you

--- sh/serve.sh
+++ sh/serve.sh
@@ -3,4 +3,4 @@
 kill -9 $(ps aux | grep '[j]ekyll' | awk '{print $2}')
 clear
 JEKYLL_ENV=development
-bundle exec jekyll serve -V --livereload --livereload-min-delay 2
+bundle exec jekyll serve -V --livereload --livereload-min-delay 2 --trace
