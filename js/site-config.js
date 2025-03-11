6891cb9e7efae4f7cb8de74b62f4b58fb9e65b7d
apperly <jim.apperly@gmail.com>
2025-03-11 23:31:03 +0000

    add blank layout and site-config.js to splat some config -> js

---
+++ js/site-config.js
@@ -0,0 +1,14 @@
+---
+layout: blank
+---
+/*
+ * Copyright (c) 2025 UsefulWorks Ltd. All Rights Reserved.
+ */
+
+/*
+ * site-config.js
+ *
+ * Splat some site configuration into the global JS scope.
+ *
+ */
+const $features = {{ site.uw.features | jsonify }};
