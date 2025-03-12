0e967d15bf3094fdedb53d5f9e04ce6e00b8600a
apperly <jim.apperly@gmail.com>
2025-03-12 15:48:30 +0000

    recaptcha progress

--- js/site-forms.js
+++ js/site-forms.js
@@ -2,18 +2,34 @@
  * Copyright (c) 2025 UsefulWorks Ltd. All Rights Reserved.
  */
 
-const Forms = {
+const Forms = (function(){
 
-    init : function() {
-        console.log("Forms.init");
-        //Forms.renderRecaptcha();
-    },
+    let _recaptcha = null;
 
-    renderRecaptcha : function() {
-        // the external recaptcha/api.js must be loaded *after* this def
-        // window.onload waits for the script to load (document on load does not)
 
-        grecaptcha.render("g-recaptcha", { "sitekey": "{{ sitekey }}" });
+    return {
+        init : function() {
+            console.log("Forms.init()");
+            _recaptcha = document.getElementById($UW.recaptcha_container);
+            if (!_recaptcha) {
+                console.warn("Recaptcha container not found");
+            }
+        },
+        renderRecaptcha : function() {
+            // the external recaptcha/api.js must be loaded *after* this def
+            // window.onload waits for the script to load (document on load does not)
+            // grecaptcha.render("g-recaptcha", { "sitekey": "{{ sitekey }}" });
+        },
+        recaptchaCallback : function(response) {
+            console.log("Forms.recaptchaCallback()");
+            //console.log(response);
+        },
     }
-};
-window.addEventListener("load", Forms.init);
+
+// end of IIFE
+}(iife = 0));
+
+// onload => init
+document.readyState == "loading"
+    ? window.addEventListener("load", Forms.init)
+    : Forms.init();
