54b12329375fd3fd144d5f5ffb4e3f6c3db33ab2
apperly <jim.apperly@gmail.com>
2025-03-17 16:10:49 +0000

    ready() not quite ready

--- js/usefulworks.js
+++ js/usefulworks.js
@@ -19,11 +19,25 @@
     // build
     factory(context);
 })(window, function (window) {
+
+    // an array-like length property
     let _length = 0;
     function setLength(l) {
         console.log(`setLength(${l})`);
         _length = l;
     }
+
+    let _isReady = false,
+        _waitingForReady = [],
+        callWhenReady = function(handler) { _waitingForReady.push(handler); },
+        doReady = function() {
+            while (_waitingForReady.length > 0) {
+                handler = _waitingForReady.shift();
+                if (typeof handler === "function") {
+                    handler();
+                }
+            }
+        };
 
     // initialize the base UsefulWorks object, which calls
     // the contructor function 'UsefulWorks.fn.init'
@@ -50,18 +64,19 @@
         each: function (target, callback) {
             console.log("UsefulWorks.each()");
 
-            // if only one argument is passed, assume it's the callback
+            // if only one argument is passed, assume that it is the callback
+            // and the target is the UsefulWorks object
             if (arguments.length === 1) {
                 callback = target;
                 target = this;
             }
 
-            // exec the callback; returning false will stop iteration
+            // define the callback
             function doCallback(index) {
                 return callback.call(target[index], index, target[index]);
             }
 
-            // iterate over the target
+            // iterate over the target; returning false will stop iteration
             if (isArrayLike(target)) {
                 // as array
                 let len = target.length;
@@ -88,18 +103,15 @@
             this.each((i, e) => e.removeEventListener(eventName, handler, false));
             return this;
         },
-        ready(callback) {
-            console.log("UsefulWorks.ready()");
-            if (!this._waitingForReady) this._waitingForReady = [];
+        ready(handler) {
+            console.log(`UsefulWorks.ready() isReady=${_isReady}`);
 
-            this._waitingForReady.push(callback);
-
+            callWhenReady(handler);
+            if (this._isReady) {
+                this.doReady();
+            }
             return this;
         },
-        //when(condition) {
-        //  console.log("UsefulWorks.when()");
-        //    return this;
-        //},
 
         // the splice function lets UsefulWorks be logged as an array in the console (???)
         splice: Array.prototype.splice,
@@ -108,7 +120,7 @@
         },
     };
 
-    // init (context ignored for now)
+    // init (context ignored, for now)
     const init = (UsefulWorks.fn.init = function (selector, context) {
         console.log(`UsefulWorks.init(): ${selector}`);
 
@@ -149,20 +161,6 @@
         return this;
     });
 
-    // test: prep ready function here
-    const beforeReady = function () {
-        document.addEventListener("DOMContentLoaded", afterReady);
-        window.addEventListener("load", afterReady);
-
-        function afterReady() {
-            document.removeEventListener("DOMContentLoaded", afterReady);
-            window.removeEventListener("load", afterReady);
-            //
-            // invoke ready()
-            // ...
-        }
-    };
-
     // assign UsefulWorks.fn.init prototype to UsefulWorks
     // prototype that contains the API methods
     init.prototype = UsefulWorks.prototype;
@@ -171,7 +169,7 @@
     window.UsefulWorks = window.u_w = UsefulWorks;
 
     // root context
-    // const _root = UsefulWorks(document);
+    const _root = UsefulWorks(document);
 
     //
     // utility functions
@@ -179,7 +177,7 @@
 
     // isArrayLike
     function isArrayLike(value) {
-        function isLength(len) {
+        function hasLength(len) {
             return typeof len === "number" && len > -1 && len % 1 === 0 && len < 4294967296;
         }
         if (!value || typeof value === "function" || value === window) {
@@ -188,35 +186,34 @@
         if (Object.prototype.toString.call(value) === "[object Array]") {
             return true;
         }
-        return isLength(value.length);
+        return hasLength(value.length);
     }
 
     // merge
     function merge(arr1, arr2) {
     }
 
-    // pinched from JQuery...
-    // completed(): the ready event handler and self cleanup method
-    function completed() {
-        document.removeEventListener("DOMContentLoaded", completed);
-        window.removeEventListener("load", completed);
-        // jQuery.ready();
-    }
+    // setup window/document ready listeners
+    (function() {
+        function completed() {
+            console.log(`UsefulWorks.completed() ${this === window ? "window" : "document"}`);
+            document.removeEventListener("DOMContentLoaded", completed);
+            window.removeEventListener("load", completed);
+            _isReady = true;
+            UsefulWorks.fn.ready();
+        }
 
-    // catch cases where $(document).ready() is called after the events have already occurred
-    if (document.readyState === "complete" ||
-        (document.readyState !== "loading" && !document.documentElement.doScroll)) {
+        // catch cases where $(document).ready() is called after the events have already occurred
+        if (document.readyState === "complete") {
+            window.setTimeout(UsefulWorks.fn.ready);
 
-        // handle asynchronously to allow scripts to delay ready
-        //window.setTimeout(jQuery.ready);
+        } else {
+            document.addEventListener("DOMContentLoaded", completed);
+            window.addEventListener("load", completed);
+        }
+        console.log("UsefulWorks(): ready listeners listening");
+    }());
 
-    } else {
-        // sse the handy event callback
-        document.addEventListener("DOMContentLoaded", completed);
-
-        // fallback to window.onload, that will always work
-        window.addEventListener("load", completed);
-    }
-
+    // expose ourself
     console.log("iife end");
 });
