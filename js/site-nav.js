8d39fdcc5e14f854d47df38c4495ac96a2e6d937
apperly <jim.apperly@gmail.com>
2025-06-26 16:07:45 +0100

    initial commit on pub2025.6.26.3

---
+++ js/site-nav.js
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2025 UsefulWorks Ltd. All Rights Reserved.
+ */
+
+const NavBar = {
+    init : function() {
+        $UW.log("NavBar.init");
+        const uwNavBar = $UW("nav.top-nav");
+        const uwWindow = $UW(window);
+        const menuToggleClass = "vertical-menu-showing";
+
+        NavBar.scrollY = window.scrollY;
+
+        const initNavMenuToggle = function () {
+            $UW.log("NavBar.initNavMenuToggle");
+            $UW("#navbar-menu-button").on("click", () => {
+                uwNavBar.toggleClass(menuToggleClass);
+            });
+        };
+
+        const initWindowScroll = function() {
+            $UW.log("NavBar.initWindowScroll");
+            const isSlim = slim => (slim > 50);
+            const handleWindowScroll = function() {
+                let scrollYNow = window.scrollY;
+                let scrollYPre = NavBar.scrollY;
+
+                let wasSlim = isSlim(scrollYPre);
+                let nowSlim = isSlim(scrollYNow);
+
+                if (wasSlim && !nowSlim) {
+                    uwNavBar.removeClass("top-nav-slim");
+                    uwNavBar.removeClass(menuToggleClass);
+               } else if (!wasSlim && nowSlim) {
+                    uwNavBar.addClass("top-nav-slim");
+                }
+
+                NavBar.scrollY = scrollYNow;
+                document.documentElement.setAttribute("data-scrollY", `${scrollYNow}px`);
+            };
+            uwWindow.on("scroll", handleWindowScroll).callLastOn();
+        };
+
+        const initDynamicNavHeight = function() {
+            $UW.log("NavBar.initDynamicNavHeight");
+            const resizer = new ResizeObserver(entries => {
+                entries.forEach(entry => {
+                    if (entry.target === uwNavBar[0]) {
+                        let heightNow = uwNavBar[0].offsetHeight;
+                        document.documentElement.style.setProperty("--uw-top-nav-height", `${heightNow}px`);
+                    }
+                });
+            });
+            resizer.observe(uwNavBar[0], { box: "border-box" });
+        };
+
+        const initMatchMedia = function() {
+            $UW.log("NavBar.initMatchMedia");
+            const handleMediaChange = function(eventOrMedia) {
+                if (!eventOrMedia.matches) {
+                    // screen is wider than 800px
+                    uwNavBar.removeClass(menuToggleClass);
+                }
+            };
+            const mediaQuery = window.matchMedia("(max-width: 800px)"); // needs to match the CSS media query
+            mediaQuery.addEventListener("change", handleMediaChange);
+            handleMediaChange(mediaQuery);
+        };
+
+        initNavMenuToggle();
+        initMatchMedia();
+        initWindowScroll();
+        initDynamicNavHeight();
+    }
+};
+
+// ready => init
+UsefulWorks(NavBar.init);
